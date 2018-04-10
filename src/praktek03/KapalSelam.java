68bd0db262069f92a5cda8f1aa459fc763387ef2
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-04-10 13:39:12 +0700

    attribute dan method

---
+++ src/praktek03/KapalSelam.java
@@ -0,0 +1,10 @@
+package praktek03;
+
+public class KapalSelam extends Kapal{
+    private String jenisKapal;
+    
+    void throttle(){
+        System.out.println("THOOOTT......");
+    }
+    
+}
