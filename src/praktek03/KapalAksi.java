2e5b70df2993b3f5e090c46ba7bcffeb6a247e48
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-04-10 13:09:10 +0700

    Instance, buat atribut dan panggil method

--- src/praktek03/KapalAksi.java
+++ src/praktek03/KapalAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class KapalAksi {
     public static void main(String[] args) {
-        
+         Kapal k = new Kapal();
+         k.merk="Titanic";
+         k.warna="Hitam";
+         k.harga=1500;
+         
+         k.cetakInfo();
     }
 }
